959ae71ceaa0b5b4f2c615e6e49580ee351a2ace
baddiezh2o <pinkalien.xx@gmail.com>
2023-05-23 16:26:47 +0800

    deleted welcome & change layout in index

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -5,9 +5,9 @@
     <title>Laravel 9 CRUD Tutorial Example</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
 </head>
-<body>
-    <div class="container mt-2">
-        <div class="row">
+<body class="">
+    <div class="container mt-2 d-flex justify-content-center align-items-center flex-column" >
+        <div class="row" style="margin-top: 200px">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
                     <h2>Laravel 9 CRUD Example Tutorial</h2>
